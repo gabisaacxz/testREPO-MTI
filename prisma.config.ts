c9cd3d74f43155f46d343e2541a7ae8bcd3254aa
Gabriel Capapas <capapasgabrielisaac19@gmail.com>
2026-02-04 16:54:35 +0800

    Update prisma.config.ts before switching to main

--- prisma.config.ts
+++ prisma.config.ts
@@ -1,12 +1,5 @@
 import { defineConfig } from "prisma/config";
 import "dotenv/config";
-import { createClient } from "@supabase/supabase-js";
-
-export const supabase = createClient(
-  process.env.NEXT_PUBLIC_SUPABASE_URL!,
-  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
-);
-
 
 export default defineConfig({
   datasource: {
